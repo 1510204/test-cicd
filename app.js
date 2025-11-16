8298f03b344877a5dc4b68f2712d170b4b2a7ba5
ncd <l96bill69l@gmail.com>
2025-11-16 14:41:37 +0700

    the first up to date

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello! Day la web tu trong Docker.\n'); // Chữ bạn muốn hiển thị
+  res.end('The first project\n'); // Chữ bạn muốn hiển thị
 });
 
 server.listen(port, () => {
